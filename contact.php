647491ec9a80722400e0717d659ec79bba57a1c7
Suvajit Sengupta <jeet_sen@yahoo.co.in>
2018-01-16 13:54:42 +0530

    Add valid email to test

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'suvajit.sengupta@gmail.com';
+$siteTitle = 'Message from gluecom.in';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
